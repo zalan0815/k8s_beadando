2abe5dd7d9594d2fae26d7945fd36c2a7b93e6d6
zalan0815 <tofalvi.zalan@students.jedlik.eu>
2026-04-24 08:17:59 +0200

    init

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// Egyszerű "adatbázis"
+const oscars = {
+  2020: "Parasite",
+  2021: "Nomadland",
+  2022: "CODA",
+  2023: "Everything Everywhere All at Once",
+  2024: "Oppenheimer"
+};
+
+app.get("/oscar/:year", (req, res) => {
+  const year = req.params.year;
+  const winner = oscars[year];
+
+  if (winner) {
+    res.json({ year, winner });
+  } else {
+    res.json({ error: "Nincs adat ehhez az évhez" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server fut a http://localhost:3000 címen");
+});
